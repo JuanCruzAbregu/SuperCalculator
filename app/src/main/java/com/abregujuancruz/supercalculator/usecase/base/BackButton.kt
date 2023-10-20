b6388f42f2fe78662e8da9a54b0139fb1fb614b2
Juan Cruz ABREGU <juan.abregu@naranjax.com>
2023-10-20 17:29:34 -0300

    Base screen, toolbar and some fixes

---
+++ app/src/main/java/com/abregujuancruz/supercalculator/usecase/base/BackButton.kt
@@ -0,0 +1,26 @@
+@file:Suppress("FunctionNaming", "TopLevelPropertyNaming")
+
+package com.abregujuancruz.supercalculator.usecase.base
+
+import androidx.compose.foundation.Image
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import com.abregujuancruz.supercalculator.R
+
+@Composable
+fun BackButton(
+    onBack: () -> Unit,
+    contentDescription: String? = null
+) {
+    IconButton(
+        onClick = { onBack() },
+    ) {
+        Image(
+            painterResource(id = R.drawable.ic_arrow_back),
+            contentDescription = contentDescription
+                ?: stringResource(id = R.string.back_btn_content_description)
+        )
+    }
+}
